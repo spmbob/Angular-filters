ca039ef916051104f3411e5bc15ef2dfdb9328a8
spmbob <urban_manki@mail.ru>
2016-10-20 09:57:01 +0300

    Angular-filters

---
+++ Number Filter/main.js
@@ -0,0 +1,8 @@
+angular.module("exampleApp",[])
+.controller("defaultCtrl", function ($scope) {
+        $scope.items = [
+            { name: "Item 1", price: 10.9, category: "Category 1", count: 100000, tax: 1.12, expiration: 10},
+            { name: "Item 2", price: 1.1, category: "Category 1", count: 8, tax: 0.55, expiration: 12},
+            { name: "Item 3", price: 2.6, category: "Category 2", count: 7, tax: 0.22, expiration: 5},
+            { name: "Item 4", price: 17.5, category: "Category 2", count: 33, tax: 2.77, expiration: 10}];
+    });
